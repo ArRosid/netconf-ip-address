d8cc147e7f143b57deb743535d80d7400ca378d5
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2018-10-05 06:03:54 +0700

    add description and subnetmask to inventory.yml

---
+++ configure_ip_address.py
@@ -0,0 +1,35 @@
+from ncclient import manager
+import xmltodict
+import json
+
+IETF_INTERFACE_TYPES = {
+        "loopback": "ianaift:softwareLoopback",
+        "ethernet": "ianaift:ethernetCsmacd"
+    }
+
+with open('inventory.yml') as f:
+	inventory = f.read()
+
+inventory_dict = yaml.load(inventory)
+
+
+netconf_template = open('ip_address_template.xml').read()
+
+
+netconf_payload = netconf_template.format(int_name='Loopback0',
+										int_desc='Loopback Address',
+										ip_addr='172.16.255.1',
+										subnetmask='255.255.255.255',
+										int_type=IETF_INTERFACE_TYPES['loopback'])
+
+print("Configuration Payload: ")
+print('-----------------------')
+print(netconf_payload)
+
+with manager.connect(host='192.168.122.10', port='830',
+					username='cisco', password='cisco',
+					hostkey_verify=False) as m:
+
+	netconf_reply = m.edit_config(netconf_payload, target='running')
+
+	print(netconf_reply)
