bc8e26c9876a1107bf767146ff6d85eaac918bbf
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2018-10-05 07:16:14 +0700

    netconf success!!

--- configure_ip_address.py
+++ configure_ip_address.py
@@ -3,35 +3,50 @@ import xmltodict
 import json
 import yaml
 
-IETF_INTERFACE_TYPES = {
-        "loopback": "ianaift:softwareLoopback",
-        "ethernet": "ianaift:ethernetCsmacd"
-    }
 
 with open('inventory.yml') as f:
 	inventory = f.read()
 
 inventory_dict = yaml.load(inventory)
-print(json.dumps(inventory_dict,indent=4))
-'''
+#print(json.dumps(inventory_dict,indent=4))
+
+device_list = inventory_dict['CORE']
+
 netconf_template = open('ip_address_template.xml').read()
 
+for device in device_list:
+	host = device['host']
+	interface_list = device['int_config']
 
-netconf_payload = netconf_template.format(int_name='Loopback0',
-										int_desc='Loopback Address',
-										ip_addr='172.16.255.1',
-										subnetmask='255.255.255.255',
-										int_type=IETF_INTERFACE_TYPES['loopback'])
+	print('----------------------------------------')
+	print(f'Configuring IP Address on {host}')
+	print('----------------------------------------')
 
-print("Configuration Payload: ")
-print('-----------------------')
-print(netconf_payload)
+	#add <config> to the payload
+	netconf_payload_list = ['<config>'] 
 
-with manager.connect(host='192.168.122.10', port='830',
-					username='cisco', password='cisco',
-					hostkey_verify=False) as m:
+	for intf in interface_list:
+		intf_netconf_payload = netconf_template.format(int_name=intf['interface'],
+												int_desc=intf['description'],
+												ip_addr=intf['ip_address'],
+												subnetmask=intf['subnetmask'],
+												int_type=intf['type'])
+		print(intf_netconf_payload)
+		#add payload for all interface
+		netconf_payload_list.append(intf_netconf_payload)
 
-	netconf_reply = m.edit_config(netconf_payload, target='running')
+	#add the close </config> to the payload
+	netconf_payload_list.append('</config>')
 
-	print(netconf_reply)
-'''
+	#join the payload_list
+	netconf_payload = ''.join(netconf_payload_list)
+
+	print(netconf_payload)
+
+	#send the payload to the host
+	with manager.connect(host=host, port='830',
+						username='cisco', password='cisco',
+						hostkey_verify=False) as m:
+
+		netconf_reply = m.edit_config(netconf_payload, target='running')
+		print(netconf_reply)
